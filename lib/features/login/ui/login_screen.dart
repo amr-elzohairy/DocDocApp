0741c5076a4b7191c631dc65dcca0c5b331199d9
Amr-Elzohairy <aelzohairy9@gmail.com>
2024-09-25 17:25:08 +0300

    creating Global form key

--- lib/features/login/ui/login_screen.dart
+++ lib/features/login/ui/login_screen.dart
@@ -1,10 +1,17 @@
+import 'package:docdoc/core/helpers/spacing.dart';
 import 'package:docdoc/core/themes/styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,11 +26,17 @@ class LoginScreen extends StatelessWidget {
                   'Welcome Back',
                   style: TextStyles.font24BlueBold,
                 ),
-                SizedBox(
-                  height: 8.h,
+                verticalsSpace(8),
+                Text(
+                  'We\'re excited to have you back, can\'t wait to \n see what you\'ve been up to since you last logged in.',
+                  style: TextStyles.font14GrayRegular,
                 ),
-                Text('We\'re excited to have you back, can\'t wait to \n see what you\'ve been up to since you last logged in.'
-                , style: TextStyles.font14GrayRegular,)
+                verticalsSpace(36),
+                Form(
+                    key: formKey,
+                    child: const Column(
+                      children: [],
+                    )),
               ],
             ),
           ),
